b71af06e6d76147bc573b157cd3c27b9bc779aea
ALDBANDA <alfred.banda1986@gmail.com>
2024-01-02 10:57:21 +0000

    'Navbar component'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
+import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 
 function App() {
  
-
   return (
     <main className='flex'>
       <Sidebar />
-      <div>
-        Components
+      <div className= 'flex flex-col flex-1 relative'>
+        <Navbar />
       </div>
     </main>
   )
