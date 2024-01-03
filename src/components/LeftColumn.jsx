f2fa46c75e7129bd4f104e0655b52ee3ca026367
ALDBANDA <alfred.banda1986@gmail.com>
2024-01-03 19:31:18 +0000

    'Tablecomponent'

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CardItem from './CardItem'
 import AreaChartComponent from './AreaChartComponent';
+import TableComponent from './TableComponent';
 
 
 const LeftColumn = () => {
@@ -13,6 +14,7 @@ const LeftColumn = () => {
          </div>
            <div className='flex-auto w-full'>
             <AreaChartComponent />
+            <TableComponent />
            </div>
       </div>
   )
